bca9b808db7c06d27d642f0f6e064f20415d2696
lucas2251 <lucascordoba2@hotmail.com>
2022-06-22 21:06:24 -0300

    subo el trabajo completo, podria tener cosas que mejorar

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = 3030
 
 app.use(express.static(path.join(__dirname,"public")))
 
-app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","index.html")))
+app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")))
 
-app.get("*",(req,res) => res.sendFile(path.join(__dirname, 'views','404.html')))
+
 
 
 app.listen(port,()=> console.log(`se levanto con el exito el servidor en http://localhost:${port}`))
