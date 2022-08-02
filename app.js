93f0f58853426556e0489ce29c30ac8f1b695232
lucas2251 <lucascordoba2@hotmail.com>
2022-08-01 21:26:43 -0300

    le doy estructura html al formulario nde register

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const port = 3030
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")))
-
+app.get("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")))
+app.get("/register",(req,res)=> res.sendFile(path.resolve(__dirname,"views","register.html")))
 
 
 
